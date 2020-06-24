51862b93c4fa474d21662e570bb9bc8d5c40a763
“gijsvanpamelen” <g.v.pamelen@gmail.com>
2020-06-24 13:28:16 +0200

    feat: growth dashboard

--- visuals/global_growth.py
+++ visuals/global_growth.py
@@ -16,22 +16,43 @@ start_date = '2020-02-01'
 
 
 # User Defined Functions
-def getBarData(n=10):
-    """
-    Get top N countries per day by total confirmed cases
-    """
+
+#def getBarData(n=10):
+#    """
+#    Get top N countries per day by total confirmed cases
+#    """
+#    query = """
+#            SELECT *
+#            FROM (
+#                SELECT
+#                    DATE(date) AS date,
+#                    country,
+#                    SUM(confirmed) AS confirmed,
+#                    ROW_NUMBER() OVER (PARTITION BY date ORDER BY SUM(confirmed) DESC) AS rnk
+#                FROM stats
+#                GROUP BY date, country) AS sub
+#            WHERE rnk <= {}
+#            """.format(n)
+#
+#    return qdb.execute_query(query)
+
+
+def getExpData():
     query = """
-            SELECT *
-            FROM (
-                SELECT
-                    DATE(date) AS date,
-                    country,
-                    SUM(confirmed) AS confirmed,
-                    ROW_NUMBER() OVER (PARTITION BY date ORDER BY SUM(confirmed) DESC) AS rnk
-                FROM stats
-                GROUP BY date, country) AS sub
-            WHERE rnk <= {}
-            """.format(n)
+        SELECT total.date,
+               total.country,
+               total_confirmed AS confirmed,
+               new_last_week,
+               ROW_NUMBER() OVER (PARTITION BY total.date ORDER BY total_confirmed DESC) as rnk
+          FROM (SELECT country,
+                       DATE(date) AS date,
+                       confirmed AS total_confirmed
+                  FROM stats ) AS total
+          JOIN (SELECT DATE(date) AS date,
+                       country,
+                       SUM(confirmed) OVER (PARTITION BY country ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS new_last_week
+                  FROM daily_stats ) AS last_week
+            ON last_week.date = total.date AND last_week.country = total.country;"""
 
     return qdb.execute_query(query)
 
@@ -57,6 +78,30 @@ def getContinentData():
     continent = qdb.execute_query(query)
     return continent
 
+def getPlotDataSource(sample_exp, countries, coutry_color_mapping, dt = '2020-05-20'):
+    """Create the required ColumnDataSource for the multi-lineplot for
+    date up till dt start_date (used in slider)
+    """
+    # get individual countries
+    #countries = sample_exp.country.unique().tolist()
+    #sample_exp[sample_exp[]]
+    # current & past top 10
+    rel_countries = list(sample_exp[(sample_exp['date'] <= dt) & (sample_exp['rnk'] <= 10)]['country'].unique())
+
+    total_conf_set, last_week_set = [], []
+    for country in rel_countries:
+        single_country = sample_exp[(sample_exp['country']==country) & (sample_exp['date'] <= dt) & (sample_exp['confirmed']>=100)]
+        total_conf_set.append(np.array(single_country.confirmed))
+        last_week_set.append(np.array(single_country.new_last_week))
+
+    # create dict for ColumnDataSource
+    data = {'xs' : total_conf_set,
+            'ys' : last_week_set,
+            'color' : [coutry_color_mapping[country] if country in countries else '#D5DBDB' for country in rel_countries],
+            'country' : rel_countries}
+
+    # color array is consistent...
+    return data
 
 def createColorMapping(countries):
     """
@@ -89,9 +134,37 @@ def updateColorMapping(coutry_color_mapping, new_countries):
 
     return coutry_color_mapping
 
+def setAxes(exp):
+    """
+    Generate labels for double logaritmic axes in a bokeh plot bases on the
+    dateset to be plotted (in exp)
+    """
+    # find the range required
+    x_max = np.ceil(np.log10(exp.confirmed.max()))
+    y_max = np.ceil(np.log10(exp.new_last_week.max()))
+
+    # generate locations
+    x_locs_raw = 10**np.arange(0,x_max+1,1)
+    x_locs = [int(i) for i in x_locs_raw]
+    y_locs_raw = 10**np.arange(0,y_max+1,1)
+    y_locs = [int(i) for i in y_locs_raw]
+
+    # generate labels
+    x_labels = ['{:,.0f}'.format(v) for v in x_locs]
+    y_labels = ['{:,.0f}'.format(v) for v in y_locs]
+
+    # get required format
+    x_set = dict(zip(x_locs, x_labels))
+    y_set = dict(zip(y_locs, y_labels))
+
+    return x_set, y_set, x_max, y_max
+
 
 # get the main dataset
-bar_data = getBarData()
+#bar_data = getBarData()
+full_exp_data = getExpData()
+bar_data = full_exp_data[full_exp_data['rnk']<=10]
+#exp_data = full_exp_data
 end_date = str(bar_data.date.max())[:10]
 continent = getContinentData()
 
@@ -114,19 +187,26 @@ def updateSlider(attr, old, new, country_color_mapping = country_color_mapping):
     if select_date == start_date:
         country_color_mapping = createColorMapping(initial_countries)
         source.data['color'] = [country_color_mapping.get(key) for key in initial_countries]
+        p_exp_source.data = getPlotDataSource(full_exp_data, initial_countries, country_color_mapping, select_date)
 
     else:
         # set colors
         new_countries = list(new_data['country']) #[::-1])
         country_color_mapping = updateColorMapping(country_color_mapping, new_countries)
         source.data['color'] = [country_color_mapping.get(key) for key in new_countries]
+        p_exp_source.data = getPlotDataSource(full_exp_data, new_countries, country_color_mapping, select_date)
 
     # update title
     rc.title.text = 'Top 10 countries with most COVID-19 cases on ' + select_date
     rc.y_range.factors = list(source.data['country'][::-1])
 
+    # update cont_plot
     cont_source.data = continent[continent['date'] == select_date]
     cont_bars.title.text = 'Confirmed cases per continent on ' + select_date
+
+    # update exp_plot
+
+
 
 
 # date-slider
@@ -170,9 +250,9 @@ source.data['color'] = [country_color_mapping.get(key) for key in initial_countr
 rc = figure(title = 'Top 10 countries with most COVID-19 cases on ' + start_date,
            y_range = FactorRange(factors = source.data['country'][::-1]),
            plot_height=300,
+           plot_width = 500,
            toolbar_location=None,
            tools="")
-
 
 # plot
 rc.hbar(y='country', right='confirmed',
@@ -188,19 +268,64 @@ rc.x_range.start = 0
 cont_data = continent[continent['date'] == start_date]
 cont_source = ColumnDataSource(cont_data)
 cont_bars = figure(title = 'Confirmed cases per continent on ' + start_date,
-                   y_range=np.sort(continent['continent'].unique()),
-                   plot_height=300, toolbar_location=None, tools="")
+                   y_range=np.sort(continent['continent'].unique())[::-1],
+                   plot_height=300, plot_width = 500,
+                   toolbar_location=None, tools="")
 
-cont_bars.hbar(y = 'continent', right = 'confirmed', source = cont_source, height=0.8)
+cont_bars.hbar(y = 'continent', right = 'confirmed',
+               fill_color = 'red', fill_alpha = 0.5,
+               line_color = None, source = cont_source, height=0.8)
+
 cont_bars.ygrid.grid_line_color = None
 cont_bars.x_range.start = 0
 cont_bars.xaxis.formatter=NumeralTickFormatter(format=",00")
 
 
+#### EXPONENTIAL PLOT
+# tooltips
+hover = HoverTool(tooltips=[('country','@country')])
+
+# create the plot
+p_exp = figure(title = 'Covid exponential growth assessment on ' + start_date,
+           x_axis_type = 'log',
+           y_axis_type = 'log',
+           x_axis_label = 'Total confirmed cases',
+           y_axis_label = 'New cases last week',
+           plot_height = 600,
+           plot_width = 700,
+           tools = [hover],
+           toolbar_location = None)
+
+# create the source
+p_exp_source = ColumnDataSource(data = getPlotDataSource(full_exp_data, initial_countries, country_color_mapping, start_date))
+
+# plot
+p_exp.multi_line(xs="xs", ys="ys", line_color="color",
+                line_width=2, source=p_exp_source)
+
+
+
+# get parameters for axes and baselines
+x_set, y_set, x_max, y_max = setAxes(full_exp_data)
+
+# get grey shaded & dashed baseline
+n = int(min(x_max,y_max))
+ln = np.logspace(0, int(n), int(n))
+p_exp.line(ln, ln, line_dash="4 4", line_width=1, color='gray')
+
+# set axes
+p_exp.x_range.start = 100
+p_exp.xaxis.major_label_overrides = x_set
+p_exp.y_range.start = 100
+p_exp.yaxis.major_label_overrides = y_set
+
+
+
 
 ### add layout here
-layout = column(rc, cont_bars, row(date_slider, button, sizing_mode='scale_width'))
-
+#layout = column(rc, cont_bars, row(date_slider, button, sizing_mode='scale_width'))
+layout = row(column(rc, cont_bars),
+             column(p_exp,row(date_slider, button, sizing_mode='scale_width')))
 # output
 curdoc().add_root(layout)
 curdoc().title = 'Covid-19'
